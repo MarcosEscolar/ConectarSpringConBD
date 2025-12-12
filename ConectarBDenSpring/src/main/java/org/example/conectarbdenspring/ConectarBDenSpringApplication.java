d19389916ae090899312417c2d9296bb36949522
MarcosEscolar <marcos.escolar24@campusfp.es>
2025-12-12 12:52:13 +0100

    Add files via upload

---
+++ ConectarBDenSpring/src/main/java/org/example/conectarbdenspring/ConectarBDenSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.conectarbdenspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectarBDenSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConectarBDenSpringApplication.class, args);
+    }
+
+}
---
+++ ConectarBDenSpring/src/test/java/org/example/conectarbdenspring/ConectarBDenSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.conectarbdenspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectarBDenSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
